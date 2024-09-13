b23dd23e7c37c0614161e8626ece7c7f5ea14b6f
kimhsbrn1 <kimhsbrgn@gmail.com>
2024-09-12 21:48:39 -0500

    added dayofweek assignment

--- lesson_04/dayOfWeek/app.js
+++ lesson_04/dayOfWeek/app.js
@@ -1,9 +1,17 @@
 document.querySelector('#check').addEventListener('click', checkDay)
 
 function checkDay() {
-  
+
+  let valid_days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
   const day = document.querySelector('#day').value
 
-  // logic goes here
+  if (!(valid_days.includes(day.toLowerCase()))) {
+    document.querySelector('#placeToSee').innerText = "Please enter valid day."
+  } else {
+    answer = day[0].toLowerCase() === 's' ? "Weekend" : "Weekday"
+    document.querySelector('#placeToSee').innerText = answer
+  }
 
 }
+
+
