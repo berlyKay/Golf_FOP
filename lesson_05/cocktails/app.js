c782630c45c9e4175ab0a107375e974f0c8e4dea
kimhsbrn1 <kimhsbrgn@gmail.com>
2024-09-14 13:46:41 -0500

    added cocktails code

--- lesson_05/cocktails/app.js
+++ lesson_05/cocktails/app.js
@@ -1,8 +1,18 @@
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+document.querySelector("button").addEventListener("click", getDrink)
+
+function getDrink () {
+  const newDrink = document.querySelector("input").value
+  // console.log(drinkName)
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${newDrink}`)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data.drinks[0])
+    document.querySelector("img").src = data.drinks[0].strDrinkThumb
+    document.querySelector(".drnkName").innerText = data.drinks[0].strDrink
+    document.querySelector(".instructions").innerText = data.drinks[0].strInstructions
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+
+}
