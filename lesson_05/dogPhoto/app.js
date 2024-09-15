503c3be2c980744d2fd74b34f5740c56d943f4de
kimhsbrn1 <kimhsbrgn@gmail.com>
2024-09-14 20:45:16 -0500

    added dogphoto assignment

--- lesson_05/dogPhoto/app.js
+++ lesson_05/dogPhoto/app.js
@@ -1,12 +1,14 @@
-fetch("https://dog.ceo/api/breeds/image/random")
+function getDog() {
+  console.log('hello')
+  fetch("https://dog.ceo/api/breeds/image/random")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data)
+      document.querySelector("img").src = data.message
+      console.log(data.message)
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
-
-
-
-    
+  }
+  
+  document.querySelector(".new_dog").addEventListener("click", getDog)
