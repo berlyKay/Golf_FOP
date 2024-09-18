a1601eaff726f2eaefd3b90afd9a7c0fe111e163
kimhsbrn1 <kimhsbrgn@gmail.com>
2024-09-18 18:37:34 -0500

    changed to no vs False

--- lesson_07/countLetters/countletters.py
+++ lesson_07/countLetters/countletters.py
@@ -2,7 +2,7 @@ def count_letters(my_list):
     my_obj = {}
     for letter in my_list:
             letter = letter.upper()
-            if (letter in my_obj) == False:
+            if letter not in my_obj:
                 my_obj[letter] = 1
             else:
                 my_obj[letter] += 1
