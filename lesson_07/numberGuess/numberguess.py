b227ca8ccc6acc679579e475ccb2573762cfca0e
kimhsbrn1 <kimhsbrgn@gmail.com>
2024-09-18 18:09:29 -0500

    added random

--- lesson_07/numberGuess/numberguess.py
+++ lesson_07/numberGuess/numberguess.py
@@ -1,25 +1,21 @@
+import random
+
+target = random.randint(1, 100) 
 
 def guess_Number():
-    target = int(input("Please enter an integer: "))
+    print(target)
     guess = int(input("Enter your guess: "))
     count = 1
-    while count < 4:
-        if guess == target:
-            if count == 1:
-                return f"You guessed it in {count} try!!"
-            else:
-                return f"You guessed it in {count} tries!"
+    while guess != target:
         if guess > target:
             guess = int(input("Too high - try again: "))
         elif guess < target:
             guess = int(input("Too low - try again: "))
         count += 1
-    print("Sorry! You weren't able to guess it in four tries.")  
-    play_again = input("Play again? Y or N ")
-    if play_again.upper() == "Y":
-        return guess_Number()
-    else:
-        return "Thank you for playing! Try again soon."
+        if guess == target:
+            if count == 1:
+                return f"You guessed it your first try!!"
+            else:
+                return f"You guessed it in {count} tries!"
 
 print(guess_Number())
-
